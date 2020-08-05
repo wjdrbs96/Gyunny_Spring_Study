63c5686f1efaa9e1db4e533955413d6f0b4ea4c3
최정균 <wjdrbs966@naver.com>
2020-08-06 01:06:37 +0900

    spring boot jwt login complete

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -13,9 +13,9 @@ public interface UserMapper {
     @Select("SELECT * FROM user")
     List<User> findAll();
 
-    @Select("SELECT * FROM user WHERE name = ${name} AND password = ${password}")
+    @Select("SELECT * FROM user WHERE name = #{name} AND password = #{password}")
     User findByNameAndPassword(@Param("name") final String name, @Param("password") final String password);
 
-    @Select("SELECT * FROM user WHERE userIdx = ${userIdx}")
+    @Select("SELECT * FROM user WHERE userIdx = #{userIdx}")
     User findByUserIdx(@Param("userIdx") int userIdx);
 }
