b7ed4d65cf877b7eca8e7b0b4d5e14768bb1af6f
최정균 <wjdrbs966@naver.com>
2020-08-29 02:34:55 +0900

    [UPDATE] JWT decode

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,21 +42,32 @@ public class JwtService {
         return null;
     }
 
+    /**
+     * 토큰 해독
+     *
+     * @param token 토큰
+     * @return 로그인한 사용자의 회원 고유 IDX
+     */
+
     public TOKEN decode(final String token) {
-        try  {
+        try {
+            // 토큰 해독 객체 생성
             final JWTVerifier jwtVerifier = require(Algorithm.HMAC256(SECRET)).withIssuer(ISSUER).build();
+            // 토큰 검증
             DecodedJWT decodedJWT = jwtVerifier.verify(token);
+            // 토큰 payload 반환, 정상적인 토큰이라면 토큰 사용자 고유 ID, 아니라면 -1
             return new TOKEN(decodedJWT.getClaim("userIdx").asLong().intValue());
-        } catch (JWTCreationException jwtCreationException) {
-            log.error(jwtCreationException.getMessage());
+        } catch (JWTVerificationException jve) {
+            log.error(jve.getMessage());
         } catch (Exception e) {
             log.error(e.getMessage());
         }
-
         return new TOKEN();
     }
 
     public static class TOKEN {
+        //토큰에 담길 정보 필드
+        //초기값을 -1로 설정함으로써 로그인 실패시 -1반환
         private int userIdx = -1;
 
         public TOKEN() {
