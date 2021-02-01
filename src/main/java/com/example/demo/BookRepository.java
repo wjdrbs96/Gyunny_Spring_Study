34e2e3205ac6fb1030fbc7c5a4d6130a911952b8
Gyunny <wjdrbs966@naver.com>
2021-02-01 14:05:59 +0900

    @Autowire update

--- src/main/java/com/example/demo/BookRepository.java
+++ src/main/java/com/example/demo/BookRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
--- src/main/java/com/example/demo/BookService.java
+++ src/main/java/com/example/demo/BookService.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class BookService {
 
+    @Autowired
     private BookRepository bookRepository;
 
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @PostConstruct
+    public void setup() {
+        System.out.println(bookRepository.getClass());
     }
 }
